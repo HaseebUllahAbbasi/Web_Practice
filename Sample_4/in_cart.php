1fa3c502abded9aac9d0ff48c5ce464bbacaa10b
Haseeb U <haseebabbasi00@gmail.com>
2021-05-17 15:09:20 +0500

    structured

--- Sample_4/in_cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
- $data = $_SESSION['data'];
- $row = $data[$_GET['id']];
- echo ++$row[1];
-    $data[$_GET['id']] = $row;
-    $_SESSION['data'] =  $data; 
-    header("Location: index.php"); 
-    ?>
